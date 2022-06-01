c04671a18495c4be4a7e3244ed94bad50a54b8d1
Danrleydev <danrleysouza53@gmail.com>
2022-06-01 09:59:45 -0300

    proteção de rotas

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,26 +1,33 @@
 import { NgModule } from '@angular/core';
+import { canActivate, redirectLoggedInTo } from '@angular/fire/auth-guard';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastroComponent } from './components/cadastro/cadastro.component';
 import { LoginComponent } from './components/login/login.component';
 import { RecuperarSenhaComponent } from './components/recuperar-senha/recuperar-senha.component';
 import { UsuarioNaoVerificadoComponent } from './components/usuario-nao-verificado/usuario-nao-verificado.component';
 
+const redirectLoggedToDiarios = () => redirectLoggedInTo(['/diarios'])
+
 const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
+    ...canActivate(redirectLoggedToDiarios),
   },
   {
     path: 'cadastro',
     component: CadastroComponent,
+    ...canActivate(redirectLoggedToDiarios),
   },
   {
     path: 'recuperar-senha',
     component: RecuperarSenhaComponent,
+    ...canActivate(redirectLoggedToDiarios),
   },
   {
     path: 'confirmar-email',
     component: UsuarioNaoVerificadoComponent,
+    ...canActivate(redirectLoggedToDiarios),
   },
 ];
 
